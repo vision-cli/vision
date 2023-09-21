b04a0a3332180e61c5450a8ea7e07cfab28bf53d
gsherrin <jennasherrington1@gmail.com>
2023-09-21 16:54:12 +0200

    refactor/plugins

--- cmd/root.go
+++ cmd/root.go
@@ -2,44 +2,60 @@ package cmd
 
 import (
 	_ "embed"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
 
 	initialise "github.com/vision-cli/vision/cmd/init"
-	"github.com/vision-cli/vision/common/execute"
-	cc "github.com/vision-cli/vision/common/plugins"
-	rp "github.com/vision-cli/vision/remote-plugins"
 )
 
 func init() {
 	rootCmd.AddCommand(initialise.RootCmd)
-	osExecutor := execute.NewOsExecutor()
-	p, err := cc.GetPlugins(osExecutor)
-	if err != nil {
-		log.Warn("cannot get plugins: %v", err)
-	}
-	for _, pl := range p {
-		cobraCmd, err := rp.GetCobraCommand(pl, osExecutor)
-		if err != nil {
-			log.Warn("cannot get cobra command %s: %v", pl.Name, err)
-		}
+	pluginArray := findVisionPlugins()
+	for _, pl := range pluginArray {
+		cobraCmd := &cobra.Command{Use: pl}
 		rootCmd.AddCommand(cobraCmd)
 	}
 }
 
 //go:embed example.txt
-var exampleText string
+var visionHelp string
 
 var rootCmd = &cobra.Command{
 	Use:     "vision",
 	Short:   "A developer productivity tool",
 	Long:    `Vision is tool to create microservice platforms and microservice scaffolding code`,
-	Example: exampleText,
+	Example: visionHelp,
 }
 
 func Execute() {
 	if err := rootCmd.Execute(); err != nil {
 		log.Error(err)
 	}
+}
+
+func findVisionPlugins() []string {
+	var pluginNameArray []string
+
+	for _, path := range strings.Split(os.Getenv("PATH"), ":") {
+		filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+			if info == nil {
+				return nil
+			}
+			if info.IsDir() {
+				return nil
+			}
+			if strings.HasPrefix(info.Name(), "vision-plugin") {
+				nameArray := strings.Split(info.Name(), "-")
+				pluginNameArray = append(pluginNameArray, nameArray[2])
+			}
+			return nil
+		})
+	}
+
+	return pluginNameArray
 }
