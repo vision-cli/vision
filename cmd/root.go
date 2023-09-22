669494796664b4b643547a893f6db3952bd099a0
lstratta <91621852+lstratta@users.noreply.github.com>
2023-09-22 16:22:54 +0100

    Refactor/plugins (#26)

--- cmd/root.go
+++ cmd/root.go
@@ -2,30 +2,185 @@ package cmd
 
 import (
 	_ "embed"
+	"encoding/json"
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"strings"
 
 	"github.com/charmbracelet/log"
 	"github.com/spf13/cobra"
+	"github.com/spf13/pflag"
 
 	initialise "github.com/vision-cli/vision/cmd/init"
-	"github.com/vision-cli/vision/common/execute"
-	cc "github.com/vision-cli/vision/common/plugins"
-	rp "github.com/vision-cli/vision/remote-plugins"
+	"github.com/vision-cli/vision/internal/plugin"
 )
 
+// Finds available plugins and initialises them into commands
 func init() {
 	rootCmd.AddCommand(initialise.RootCmd)
-	osExecutor := execute.NewOsExecutor()
-	p, err := cc.GetPlugins(osExecutor)
-	if err != nil {
-		log.Warn("cannot get plugins: %v", err)
-	}
-	for _, pl := range p {
-		cobraCmd, err := rp.GetCobraCommand(pl, osExecutor)
+	rootCmd.Flags().AddFlagSet(initVisionFlags())
+	plugins := findVisionPlugins()
+	for _, plugin := range plugins {
+		cmd, err := createCommand(plugin)
 		if err != nil {
-			log.Warn("cannot get cobra command %s: %v", pl.Name, err)
+			// TODO(steve): handle broken commands, maybe a vision doctor command???
+			log.Error(plugin.FullPath, err)
+			continue
 		}
-		rootCmd.AddCommand(cobraCmd)
+		rootCmd.AddCommand(cmd)
 	}
+}
+
+// createCommand takes in a plugin and returns a cobra command to interact with that plugin
+func createCommand(p pluginPath) (*cobra.Command, error) {
+	exe := plugin.NewExecutor(p.FullPath)
+	info, err := exe.Info()
+	if err != nil {
+		return nil, err
+	}
+	version, err := exe.Version()
+	if err != nil {
+		return nil, err
+	}
+
+	cobraCmd := &cobra.Command{
+		Use:     p.Name,
+		Version: version.SemVer,
+		Short:   info.ShortDescription,
+		Long:    info.LongDescription,
+		RunE:    createPluginCommandHandler(p),
+	}
+
+	return cobraCmd, nil
+}
+
+func createPluginCommandHandler(p pluginPath) func(cmd *cobra.Command, args []string) error {
+	return func(cmd *cobra.Command, args []string) error {
+		if len(args) < 1 { // prevents index out of range
+			log.Warnf("No argument provided. Try: \n\t\n vision %v -v", cmd.Use)
+		}
+		exe := plugin.NewExecutor(p.FullPath)
+		switch args[0] {
+		case "init":
+			i, err := exe.Init()
+			if err != nil {
+				return err
+			}
+			// TODO merge into vision config
+			mergeConfigs(p.Name, i.Config)
+		}
+		return nil
+	}
+}
+
+func mergeConfigs(pluginName string, config any) error {
+	writeSuccess := false
+	isTruncated := false
+	f, err := os.OpenFile("vision.json", os.O_RDWR, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	b, err := io.ReadAll(f)
+	if err != nil {
+		return err
+	}
+	defer func() {
+		// defensive coding to keep a clone of the original data
+		var originalData []byte
+		copy(originalData, b)
+		if !writeSuccess && isTruncated {
+			_, err = f.Write(originalData)
+			if err != nil {
+				log.Fatal(err)
+			}
+		}
+	}()
+
+	var configBytes map[string]any
+	err = json.Unmarshal(b, &configBytes)
+	if err != nil {
+		return err
+	}
+
+	configBytes[pluginName] = config
+	err = f.Truncate(0)
+	if err != nil {
+		return err
+	}
+	isTruncated = true
+	_, err = f.Seek(0, 0)
+	if err != nil {
+		return err
+	}
+	enc := json.NewEncoder(f)
+	enc.SetIndent("", "  ")
+	err = enc.Encode(configBytes)
+	if err != nil {
+		return err
+	}
+
+	writeSuccess = true
+	return nil
+}
+
+func initVisionFlags() *pflag.FlagSet {
+	fs := pflag.NewFlagSet("vision", 1)
+	return fs
+}
+
+type pluginPath struct {
+	Name     string
+	Version  string
+	FullPath string
+}
+
+// Searches all dirs in the PATH envar to find binaries with specific vision formatting and assigns them to a map.
+// The formatting is `vision-plugin-[plugin-name]-[version-number]`
+func findVisionPlugins() []pluginPath {
+	const prefix = "vision-plugin"
+
+	var plugins []pluginPath
+
+	sysPath := os.Getenv("PATH")
+	paths := strings.Split(sysPath, ":")
+
+	m := make(map[string]struct{})
+	for _, p := range paths {
+		m[p] = struct{}{}
+	}
+	var uniqPaths []string
+	for k := range m {
+		uniqPaths = append(uniqPaths, k)
+	}
+	paths = uniqPaths
+
+	for _, path := range uniqPaths {
+		filepath.Walk(path, func(path string, info fs.FileInfo, err error) error {
+			if info == nil {
+				return nil
+			}
+			if info.IsDir() {
+				return nil
+			}
+			if strings.HasPrefix(info.Name(), prefix) {
+				pluginSplit := strings.Split(info.Name(), "-")
+				if len(pluginSplit) == 4 { // only process correctly named plugins
+					name, version := pluginSplit[2], pluginSplit[3]
+					plugins = append(plugins, pluginPath{
+						Name:     name,
+						Version:  version,
+						FullPath: path,
+					})
+				}
+			}
+			return nil
+		})
+	}
+	return plugins
 }
 
 //go:embed example.txt
@@ -34,7 +189,7 @@ var exampleText string
 var rootCmd = &cobra.Command{
 	Use:     "vision",
 	Short:   "A developer productivity tool",
-	Long:    `Vision is tool to create microservice platforms and microservice scaffolding code`,
+	Long:    `Vision is a tool to create microservice platforms and microservice scaffolding code`,
 	Example: exampleText,
 }
 
---
+++ plugins/sample/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"os"
+)
+
+func main() {
+	switch os.Args[1] {
+	case "info":
+		json.NewEncoder(os.Stdout).Encode(map[string]string{
+			"short_description": "sample plugin short desc",
+		})
+	case "version":
+		json.NewEncoder(os.Stdout).Encode(map[string]string{
+			"sem_ver": "v0.0.1",
+		})
+	case "init":
+		json.NewEncoder(os.Stdout).Encode(map[string]any{
+			"config": map[string]string{
+				"subscriptionid": "value",
+			},
+		},
+		)
+	}
+}
