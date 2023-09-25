849fe585656add67a258d8d0e92ed55d731faff1
gsherrin <jennasherrington1@gmail.com>
2023-09-25 12:40:46 +0200

    push for luke

--- cmd/doctor/root.go
+++ cmd/doctor/root.go
@@ -23,6 +23,8 @@ var RootCmd = &cobra.Command{
 
 var healthRecord []string
 
+// Doctor command looks through available plugins and checks for plugin health.
+// If plugins commands are missing or incomplete, doctor returns them as faulty with a reason and prints them out.
 var doctorCommand = func(cmd *cobra.Command, args []string) error {
 	plugins := FindVisionPlugins()
 
@@ -33,37 +35,62 @@ var doctorCommand = func(cmd *cobra.Command, args []string) error {
 	for _, plug := range plugins {
 		// call each of the built in commands
 		exe := plugin.NewExecutor(plug.FullPath)
+		var reason string
 		info, err := exe.Info()
+
 		if err != nil {
-			healthCheck := fmt.Sprintf("Info is faulty for plugin: %v", plug.Name)
-			healthRecord = append(healthRecord, healthCheck)
-		} else if info.ShortDescription == "" || info.LongDescription == "" {
-			healthCheck := fmt.Sprintf("Info is faulty for plugin: %v", plug.Name)
-			healthRecord = append(healthRecord, healthCheck)
+			reason = fmt.Sprintf("%v", err)
+			addToHealthRecord(plug.Name, "Info", reason)
+		} else if info.ShortDescription == "" {
+			reason = "short description missing"
+			addToHealthRecord(plug.Name, "Info", reason)
+		} else if info.LongDescription == "" {
+			reason = "long description missing"
+			addToHealthRecord(plug.Name, "Info", reason)
 		}
 
 		ini, err := exe.Init()
 		if err != nil {
-			healthCheck := fmt.Sprintf("Init is faulty for plugin: %v", plug.Name)
-			healthRecord = append(healthRecord, healthCheck)
-		} else if ini.Config == "" || ini.Config == nil {
-			healthCheck := fmt.Sprintf("Init is faulty for plugin: %v", plug.Name)
-			healthRecord = append(healthRecord, healthCheck)
+			reason = fmt.Sprintf("%v", err)
+			addToHealthRecord(plug.Name, "Init", reason)
+		} else if ini.Config == "" {
+			reason = "config empty"
+			addToHealthRecord(plug.Name, "Init", reason)
+		} else if ini.Config == nil {
+			reason = "config missing"
+			addToHealthRecord(plug.Name, "Init", reason)
 		}
 
 		vers, err := exe.Version()
 		if err != nil {
-			healthCheck := fmt.Sprintf("Version is faulty for plugin: %v", plug.Name)
-			healthRecord = append(healthRecord, healthCheck)
+			reason = fmt.Sprintf("%v", err)
+			addToHealthRecord(plug.Name, "Version", reason)
 		} else if vers.SemVer == "" {
-			healthCheck := fmt.Sprintf("Version is faulty for plugin: %v", plug.Name)
-			healthRecord = append(healthRecord, healthCheck)
+			reason = "semantic version missing"
+			addToHealthRecord(plug.Name, "Version", reason)
 		}
 	}
-	for _, hr := range healthRecord {
-		log.Warn(hr)
-	}
+	healthRecordPrinter(healthRecord)
 	return nil
+}
+
+func addToHealthRecord(pluginName string, command string, reason string) {
+	healthCheck := fmt.Sprintf("%v is faulty. Reason: %v", command, reason)
+	healthRecord = append(healthRecord, pluginName, healthCheck)
+}
+
+func healthRecordPrinter(healthRecord []string) {
+	var curPlugin string
+	for n, hr := range healthRecord {
+		if n == 0 || n%2 == 0 {
+			if curPlugin != healthRecord[n] {
+				curPlugin = healthRecord[n]
+				fmt.Printf("\nDetails for plugin: %v\n", curPlugin)
+			}
+		} else {
+			log.Warn(hr)
+		}
+	}
 }
 
 type PluginPath struct {
