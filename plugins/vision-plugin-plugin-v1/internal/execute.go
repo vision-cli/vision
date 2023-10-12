8df778b881eb26496b4993b16ddbb84042efbcec
lstratta <lstratta@digitalfutures.com>
2023-10-12 15:16:36 +0100

    trying different move commands

--- plugins/vision-plugin-plugin-v1/internal/execute.go
+++ plugins/vision-plugin-plugin-v1/internal/execute.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"fmt"
+	"os"
 	"os/exec"
 	"path/filepath"
 )
@@ -62,10 +63,17 @@ func (exe *Executor) UpdateByCurl() error {
 		return fmt.Errorf("unzipping latest binary %v", err)
 	}
 
-	_, err = exec.Command("mv", "/tmp/rover_0.3.3_darwin_arm64/rover_v0.3.3", goBin).Output()
+	// err = os.Rename("/tmp/rover_0.3.3_darwin_arm64/rover_v0.3.3", "/Users/luke/rover_v0.3.3")
+	err = os.Rename("/Users/luke/test-file", "/Users/luke/rover_v0.3.3")
+
+	// cmd = "mv /tmp/rover_0.3.3_darwin_arm64/rover_v0.3.3 /Users/luke/rover_v0.3.3"
+	// _, err = exec.Command("bash", "-c", cmd).Output()
 	if err != nil {
-		return fmt.Errorf("moving latest binary to GOBIN %v", err)
+		fmt.Printf("moving latest binary to GOBIN %v", err)
+		return err
 	}
+
+	// fmt.Println(string(b))
 
 	return nil
 }
@@ -74,19 +82,44 @@ func (exe *Executor) UpdateByCurl() error {
 // method 2: run go get @latest
 // method 3: run curl command for latest
 // deciding on all three: method 2 first, if that fails, method 1, then method 3
-// curl -L https://github.com/charmbracelet/log/archive/refs/tags/v0.2.4.tar.gz > charmbracelet-v0.2.4.tar.gz
+// curl -OL https://github.com/charmbracelet/log/archive/refs/tags/v0.2.4.tar.gz > charmbracelet-v0.2.4.tar.gz
 
 func findArch() (string, error) {
-	cmd := exec.Command("uname", "-am")
-	b, err := cmd.Output()
-	if err != nil {
-		return "", err
-	}
+	// switch runtime.GOOS {
+	// case "windows":
+	// 	dir = Getenv("LocalAppData")
+	// 	if dir == "" {
+	// 		return "", errors.New("%LocalAppData% is not defined")
+	// 	}
 
-	// regex of string to find type and arch
+	// case "darwin", "ios":
+	// 	dir = Getenv("HOME")
+	// 	if dir == "" {
+	// 		return "", errors.New("$HOME is not defined")
+	// 	}
+	// 	dir += "/Library/Caches"
+
+	// case "plan9":
+	// 	dir = Getenv("home")
+	// 	if dir == "" {
+	// 		return "", errors.New("$home is not defined")
+	// 	}
+	// 	dir += "/lib/cache"
+
+	// default: // Unix
+	// 	dir = Getenv("XDG_CACHE_HOME")
+	// 	if dir == "" {
+	// 		dir = Getenv("HOME")
+	// 		if dir == "" {
+	// 			return "", errors.New("neither $XDG_CACHE_HOME nor $HOME are defined")
+	// 		}
+	// 		dir += "/.cache"
+	// 	}
+	// }
+
 	// return eg: linux/amd64, darwin/arm64
 
-	return string(b), nil
+	return "", nil
 }
 
 // b, err = exec.Command("curl", downloadUrl, "|", "grep", "browser_download_url", "|", "grep", "darwin_arm64", "|", "cut", "-d", `'"'`, "-f", "4").Output()
